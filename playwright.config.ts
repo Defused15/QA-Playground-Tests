900b6c646357397f7fff552a0972c58aa07cdf82
Roberto Castillo <defused15@gmail.com>
2025-07-10 21:07:58 -0600

    Github Generate QR images

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,11 +24,11 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   snapshotDir: './__snapshots__',
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
      baseURL: 'https://qaplayground.dev',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     viewport: { width: 1440, height: 900 },
